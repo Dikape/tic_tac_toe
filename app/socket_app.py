a39d92e99ba5ab28762f54f36fcccb7175728541
Dmytro Kasianchyk <dikape03@gmail.com>
2018-02-18 15:19:21 +0200

    add draw game

--- app/socket_app.py
+++ app/socket_app.py
@@ -4,6 +4,22 @@ from app import models
 from .calculations import check_winner
 
 
+def get_game_result(step, member):
+    response = {'message': 'saved'}
+    game = member.game
+    all_steps = member.steps.order_by(models.Step.step_number)
+    all_steps_current_symbol = all_steps.filter_by(value=step.value).\
+        with_entities(models.Step.x_coordinate, models.Step.y_coordinate).all()
+
+    step_coordinate = (step.x_coordinate, step.y_coordinate)
+    is_winner = check_winner(all_steps_current_symbol, step_coordinate)
+
+    if is_winner:
+        response['message'] = '{0} winner!'.format(step.value)
+    elif len(all_steps.all()) == game.size*game.size:
+        response['message'] = 'Draw game!'
+    return response
+
 @sio.on('connect', namespace='/hot_seat')
 def connect(sid, environ):
     pass
@@ -11,23 +27,14 @@ def connect(sid, environ):
 
 @sio.on('step', namespace='/hot_seat')
 def step(sid, data):
-    current_symbol = data['currentSymbol']
     member = models.Member.query.filter_by(game_id=data['gameId'], user_id=data['userId']).first()
     step = models.Step(step_number=data['stepNumber'],
                        x_coordinate=data['x'],
                        y_coordinate=data['y'],
-                       value=current_symbol,
+                       value=data['currentSymbol'],
                        member_id=member.id)
     step.save()
-    step_coordinate = (step.x_coordinate, step.y_coordinate)
-    all_steps = member.steps.filter_by(value=current_symbol).\
-        order_by(models.Step.step_number).\
-        with_entities(models.Step.x_coordinate, models.Step.y_coordinate).all()
-
-    is_winner = check_winner(all_steps, step_coordinate)
-    response = {'message': 'saved'}
-    if is_winner:
-        response['message'] = '{0} winner!'.format(current_symbol)
+    response = get_game_result(step, member)
     sio.emit('step_result', response, namespace='/hot_seat', room=sid)
 
 
