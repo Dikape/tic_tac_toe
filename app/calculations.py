fe79024a34ce5b9997a28d602e0de933f8e297d5
Dmytro Kasianchyk <dikape03@gmail.com>
2018-02-18 14:10:31 +0200

    change check winner

--- app/calculations.py
+++ app/calculations.py
@@ -15,30 +15,21 @@ def check_winner(all_steps, current_step):
 
 def find_five_in_row(step, direction, all_steps):
     number_in_row = 1
-    current_step = step
-    for i in range(4):
-        current_step = tuple(map(lambda t1, t2: t1+t2, current_step, direction))
-        if current_step in all_steps:
-            number_in_row += 1
-        else:
-            break
-    current_step = step
-    for i in range(4):
-        current_step = tuple(map(lambda t1, t2: t1 - t2, current_step, direction))
-        if current_step in all_steps:
-            number_in_row += 1
-        else:
-            break
+    for i in check_next_cell(step, direction, all_steps):
+        number_in_row += i
+    direction = tuple(map(lambda t: t*(-1), direction))
+    for i in check_next_cell(step, direction, all_steps):
+        number_in_row += i
     if number_in_row >= 5:
         return True
     return False
 
-# 1. Обираємо точку
-# 2. Перевірки вправо/вліво, вверх/вниз, права на ліво діагональ, ліва на право діаг
-# 3. Робимо крок від точки, перевіряємо чи є така точка, якщо є вертаємо +1
-# 4. Якщо нема крок в іншу сторону і повторяємо 3
-#
-# По горизонталі крок вбік: (0,1)/(0,-1)
-# По вертикалі крок верх/вниз: (1,0)/(-1,0)
-#  ліва на право діаг (-1, -1)/(1,1)
-# права на ліво діагональ (-1, 1)/(1,-1)
+
+def check_next_cell(step, direction, all_steps):
+    current_step = step
+    for i in range(4):
+        current_step = tuple(map(lambda t1, t2: t1+t2, current_step, direction))
+        if current_step in all_steps:
+            yield 1
+        else:
+            break
